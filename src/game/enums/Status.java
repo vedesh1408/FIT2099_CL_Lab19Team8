af2fb3dd05ceedde25b838486afc9e75b9525d7c
vapp0002 <vapp0002@student.monash.edu>
2022-05-02 20:03:05 +1000

    Implemeted magical items. "Note: Still need to test jumpActorAction"

--- src/game/enums/Status.java
+++ src/game/enums/Status.java
@@ -12,6 +12,6 @@ public enum Status {
     KILL_KOOPA, // use this to indicate what can kill a koopa
     FERTILE, // used to indicate this ground is dirt and can grow a new sprout
     HAS_WRENCH, //use this to indicate if player has a wrench to kill a koopa
-    INVINCIBILITY,
+    INVINCIBILITY, //use this to indicate if player is invincible
     CAN_JUMP,
 }
