c80c16b85d9acd2cebe5663a1fb5bf6883e5dab1
Oskar Hosken <oskarhosken@gmail.com>
2022-05-02 12:06:43 +1000

    Wrench implements BuyableItem

--- src/game/implemetedItems/Wrench.java
+++ src/game/implemetedItems/Wrench.java
@@ -2,14 +2,19 @@ package game.implemetedItems;
 
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.weapons.Weapon;
+import game.BuyableItem;
 import game.enums.Status;
 
-public class Wrench extends Item {
+public class Wrench extends Item implements BuyableItem {
 
     public Wrench() {
         super("Wrench", 'w', true);
         this.addCapability(Status.KILL_KOOPA);
 
+    }
+
+    public int getPrice() {
+        return 200;
     }
 
 
