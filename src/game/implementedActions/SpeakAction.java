b1756cb6167e49f9dc627825f4233a420bdda737
Oskar Hosken <oskarhosken@gmail.com>
2022-05-17 15:07:06 +1000

    Toad speaking updated.

--- src/game/implementedActions/SpeakAction.java
+++ src/game/implementedActions/SpeakAction.java
@@ -1,9 +1,14 @@
 package game.implementedActions;
 
 import edu.monash.fit2099.engine.actors.*;
+import edu.monash.fit2099.engine.items.Item;
+
+import java.util.ArrayList;
+
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.Utils;
+import game.enums.Status;
 
 
 public class SpeakAction extends Action {
@@ -23,41 +28,28 @@ public class SpeakAction extends Action {
      */
     @Override
     public String execute(Actor actor, GameMap map) {
+        ArrayList<String> lines = new ArrayList<String>();
+        lines.add("The Princess is depending on you! You are our only hope.");
+        lines.add("Being imprisoned in these walls can drive a fungus crazy :(");
+        lines.add("You might need a wrench to smash Koopa's hard shells.");
+        lines.add("You better get back to finding the Power Stars.");
+        lines.add("You may stumble upon a key that unlocks that mysterious door.");
 
-        int choice = Utils.ranNum(4);
-        for (int i = 0; i < actor.getInventory().size(); i++) {
-            if (actor.getInventory().get(i).toString().equals("Wrench") && actor.getInventory().get(i).toString().equals("Power Star")) {
-                return switch (choice) {
-                    case 0 -> "The Princess is depending on you! You are our only hope.";
-                    default -> "Being imprisoned in these walls can drive a fungus crazy :(";
-                };
-
-            } else if (actor.getInventory().get(i).toString().equals("Power Star")) {
-
-                return switch (choice) {
-                    case 0 -> "You might need a wrench to smash Koopa's hard shells.";
-                    case 1 -> "The Princess is depending on you! You are our only hope.";
-                    default -> "Being imprisoned in these walls can drive a fungus crazy :(";
-                };
-            } else if (actor.getInventory().get(i).toString().equals("Wrench")) {
-
-                return switch (choice) {
-                    case 0 -> "You better get back to finding the Power Stars.";
-                    case 1 -> "The Princess is depending on you! You are our only hope.";
-                    default -> "Being imprisoned in these walls can drive a fungus crazy :(";
-
-                };
-            } else {
-                return switch (choice) {
-                    case 0 -> "You might need a wrench to smash Koopa's hard shells.";
-                    case 1 -> "You better get back to finding the Power Stars.";
-                    case 2 -> "The Princess is depending on you! You are our only hope.";
-                    default -> "Being imprisoned in these walls can drive a fungus crazy :(";
-                };
+        for (Item item: actor.getInventory()) {
+            if (item.toString() == "Power Star" || actor.hasCapability(Status.INVINCIBILITY)) {
+                lines.remove(3);
+            }
+            else if (item.toString() == "Wrench") {
+                lines.remove(2);
+            }
+            else if (item.toString() == "Secret Key" || actor.hasCapability(Status.HAS_KEY)) {
+                lines.remove(4);
             }
         }
-        return "Being imprisoned in these walls can drive a fungus crazy :(";
+        int rand = Utils.ranNum(lines.size());
+        return lines.get(rand);
     }
+
 
     /**
      * This action that is going to be spoken
@@ -70,3 +62,41 @@ public class SpeakAction extends Action {
         return actor + " talks with " + target;
     }
 }
+
+
+
+
+// int choice = Utils.ranNum(4);
+// for (int i = 0; i < actor.getInventory().size(); i++) {
+//     if (actor.getInventory().get(i).toString().equals("Wrench") && actor.getInventory().get(i).toString().equals("Power Star")) {
+//         return switch (choice) {
+//             case 0 -> "The Princess is depending on you! You are our only hope.";
+//             default -> "Being imprisoned in these walls can drive a fungus crazy :(";
+//         };
+
+//     } else if (actor.getInventory().get(i).toString().equals("Power Star")) {
+
+//         return switch (choice) {
+//             case 0 -> "You might need a wrench to smash Koopa's hard shells.";
+//             case 1 -> "The Princess is depending on you! You are our only hope.";
+//             default -> "Being imprisoned in these walls can drive a fungus crazy :(";
+//         };
+//     } else if (actor.getInventory().get(i).toString().equals("Wrench")) {
+
+//         return switch (choice) {
+//             case 0 -> "You better get back to finding the Power Stars.";
+//             case 1 -> "The Princess is depending on you! You are our only hope.";
+//             default -> "Being imprisoned in these walls can drive a fungus crazy :(";
+
+//         };
+//     } else {
+//         return switch (choice) {
+//             case 0 -> "You might need a wrench to smash Koopa's hard shells.";
+//             case 1 -> "You better get back to finding the Power Stars.";
+//             case 2 -> "The Princess is depending on you! You are our only hope.";
+//             default -> "Being imprisoned in these walls can drive a fungus crazy :(";
+//         };
+//     }
+// }
+// return "Being imprisoned in these walls can drive a fungus crazy :(";
+// }
