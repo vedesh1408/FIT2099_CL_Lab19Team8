8516a6763b49dd373d75a22d4d982b9e85a25acd
vapp0002 <vapp0002@student.monash.edu>
2022-05-23 01:47:51 +1000

    fixed block

--- src/game/implementedactions/DestroyMBlockAction.java
+++ src/game/implementedactions/DestroyMBlockAction.java
@@ -34,44 +34,43 @@ public class DestroyMBlockAction extends Action {
         // If the player does not have the wrench, exit the function with a different return message. Else continue through the function
         if (!actor.hasCapability(Status.HAS_WRENCH)) {
             return actor + " punched the block, but it did nothing! Maybe he can break it with a wrench...";
-        }
-        // Set the ground to dirt
-        this.blockLocation.setGround(new Dirt());
+        } else {
+            // Set the ground to dirt
+            this.blockLocation.setGround(new Dirt());
 
-        // Spawn item rewards from mystery block (chance)
-        int itemRNG = Utils.ranNum(5);
-        switch (itemRNG)
-        {
-            // Intentional withhold of breaks to trigger multiple outcomes.
-            case 0:
-                // 1/5 to get 1 of each item
-                this.blockLocation.addItem(new PowerStar());
-            case 1:
-                // 1/5 to get 1 super mushroom
-                this.blockLocation.addItem(new SuperMushroom());
-                break;
-            case 2:
-                // 1/5 to get 2 super mushrooms
-                this.blockLocation.addItem(new SuperMushroom());
-                this.blockLocation.addItem(new SuperMushroom());
-                break;
-            case 3:
-                // 1/5 to get 1 power star
-                this.blockLocation.addItem(new PowerStar());
-                break;
-            default:
-                // 1/5 to get no items (case 4)
-        }
+            // Spawn item rewards from mystery block (chance)
+            int itemRNG = Utils.ranNum(5);
+            switch (itemRNG) {
+                // Intentional withhold of breaks to trigger multiple outcomes.
+                case 0:
+                    // 1/5 to get 1 of each item
+                    this.blockLocation.addItem(new PowerStar());
+                case 1:
+                    // 1/5 to get 1 super mushroom
+                    this.blockLocation.addItem(new SuperMushroom());
+                    break;
+                case 2:
+                    // 1/5 to get 2 super mushrooms
+                    this.blockLocation.addItem(new SuperMushroom());
+                    this.blockLocation.addItem(new SuperMushroom());
+                    break;
+                case 3:
+                    // 1/5 to get 1 power star
+                    this.blockLocation.addItem(new PowerStar());
+                    break;
+                default:
+                    // 1/5 to get no items (case 4)
+            }
 
-        // Spawn coin rewards from mystery block (chance for coin value)
-        int coinRNG = Utils.ranNum(3);
-        switch (coinRNG)
-        {
-            case 0 -> this.blockLocation.addItem(new Coin(20, blockLocation.x(), blockLocation.y()));
-            case 1 -> this.blockLocation.addItem(new Coin(50, blockLocation.x(), blockLocation.y()));
-            case 2 -> this.blockLocation.addItem(new Coin(100, blockLocation.x(), blockLocation.y()));
-        }
 
+            // Spawn coin rewards from mystery block (chance for coin value)
+            int coinRNG = Utils.ranNum(3);
+            switch (coinRNG) {
+                case 0 -> this.blockLocation.addItem(new Coin(20, blockLocation.x(), blockLocation.y()));
+                case 1 -> this.blockLocation.addItem(new Coin(50, blockLocation.x(), blockLocation.y()));
+                case 2 -> this.blockLocation.addItem(new Coin(100, blockLocation.x(), blockLocation.y()));
+            }
+        }
         return this.menuDescription(actor);
     }
 
